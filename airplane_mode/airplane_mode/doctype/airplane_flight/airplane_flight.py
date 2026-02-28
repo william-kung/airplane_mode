70e889acb941893876ba26396b83cf5723c866fc
william-kung <william0129@gmail.com>
2026-02-28 11:05:33 +0800

    change flight status to complete after submission

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,9 +2,9 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class AirplaneFlight(Document):
-	# TODO: Set the Status field of the Airplane Flight DocType to Completed after the document is submitted.
-	pass
+class AirplaneFlight(WebsiteGenerator):
+	def before_submit(self):
+		self.status = "Completed"
