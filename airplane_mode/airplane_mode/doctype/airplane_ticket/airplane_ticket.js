43b5f3c4c9f041680f817b8632a72753d2ba3667
william-kung <william0129@gmail.com>
2026-03-01 21:31:13 +0800

    complete Day 2 assignment

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,23 +1,8 @@
 // Copyright (c) 2026, DDR and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Airplane Ticket", {
-	refresh(frm) {
-        if (frm.doc.status !== "Booked") {
-            frm.add_custom_button("Accept", ()=>{
-                // console.log("button pressed");
-                console.log(frm.doc.passenger);
-                // for (d in frm.doc) {
-                //     console.log(d);
-                // }
-                // // status => Booked
-                // frm.set_value("status", "Booked");
-                // frm.save();
-                // save the form
-            })
-        }
-	},
-    status(frm) {
-        frm.refresh();
-    }
-});
+// frappe.ui.form.on("Airplane Ticket", {
+// 	refresh(frm) {
+
+//     }
+// });
