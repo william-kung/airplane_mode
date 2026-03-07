6b59532dee306037a55843998456b4efa5b8d625
william-kung <william0129@gmail.com>
2026-03-07 19:46:07 +0800

    add auto data push from airplane flight to airplane ticket for related change

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -15,20 +15,29 @@ class AirplaneFlight(WebsiteGenerator):
 		self.push_changes_to_tickets()
 
 	def push_changes_to_tickets(self):
-		tickets = frappe.db.get_list(
-			'Airplane Ticket', 
-			pluck='name',
-			filters={
-				'flight': self.name
-			})
-		for t in tickets:
-			doc = frappe.get_doc('Airplane Ticket', t)
-			# doc.source_airport_code = self.source_airport_code
-			# doc.source_gate_number = self.source_gate_number
-			# doc.destination_airport_code = self.destination_airport_code
-			# doc.destination_gate_number = self.destination_gate_number
-			# doc.departure_date = self.date_of_departure
-			# doc.departure_time = self.time_of_departure
-			# doc.duration_of_flight = self.duration
-			doc.save()
-		frappe.msgprint(f"{len(tickets)} tickets was updated")
+		# validate if the following ticket related fields are changed.  Avoid unnecessary DB operations.
+		fields_to_check = [
+			'source_gate_number',
+			'source_airport_code',
+			'destination_airport_code',
+			'destination_gate_number',
+			'date_of_departure',
+			'time_of_departure',
+			'duration',
+		]
+		old_doc = self.get_doc_before_save()
+		if any(getattr(old_doc, field) != getattr(self, field) and # compare value before and after
+			str(getattr(old_doc, field)) != str(getattr(self, field)) # compare string version of that value e.g. datetime, trigger action only if both are False.
+			for field in fields_to_check): 
+			# if ticket related fields are changed push changes to ticket.
+			tickets = frappe.db.get_list(
+				'Airplane Ticket', 
+				pluck='name',
+				filters={
+					'flight': self.name
+				})
+			# Open and save Airplane Ticket doctype to trigger the update of all related fields.
+			for t in tickets:
+				doc = frappe.get_doc('Airplane Ticket', t)
+				doc.save()
+			frappe.msgprint(f"{len(tickets)} tickets was updated")
