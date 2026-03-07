665a8ca4e607254edfe0b639b7bf701e169f6388
william-kung <william0129@gmail.com>
2026-03-07 22:57:11 +0800

    add flight calendar view

---
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight_calendar.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2026, DDR and contributors
+// For license information, please see license.txt
+
+frappe.views.calendar["Airplane Flight"] = {
+    field_map: {
+		start: 'date_of_departure',
+		end: 'date_of_departure',
+		id: 'name',
+		title: 'airplane',
+	},
+    gantt: true
+};
