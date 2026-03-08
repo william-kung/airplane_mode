e80733302210dc11b02754fd8595fdcf4bdbd9cb
william-kung <william0129@gmail.com>
2026-03-08 16:20:15 +0800

    add default rental rate and calculation logic

--- airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.js
+++ airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2026, DDR and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop Rental Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shop Rental Contract", {
+	refresh(frm) {
+        if(!frm.doc.rent_per_square_meter) {
+            frappe.db.get_single_value("Airport Shop Rental Settings", "standard_rate")
+            .then(val => {
+                const standard_rate = val;
+                frm.set_value("rent_per_square_meter", standard_rate);
+            });
+        }
+	},
+    calculate_rent_amount(frm){      
+        const rent_per_square_meter = frm.doc.rent_per_square_meter;
+        const area = frm.doc.area;
+        const rent_amount = rent_per_square_meter * area;
+        frm.set_value("rent_amount", rent_amount);
+    }
+});
--- airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.py
+++ airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2026, DDR and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ShopRentalContract(Document):
-	pass
+	def validate(self):
+		self.validate_rent_amount()
+
+	def validate_rent_amount(self):
+		rate = self.rent_per_square_meter
+		area = self.area
+
+		rent_amount = rate * area
+		if self.rent_amount != rent_amount:
+			frappe.throw(
+				title="Error",
+				msg="Please check Rent Amount."
+			)
