592bdcdbaeb7576f05844276bf6c99a8b5bdc632
william-kung <william0129@gmail.com>
2026-03-09 00:17:53 +0800

    airport rental income log add auto date input

--- airplane_mode/shop_lease/doctype/airport_rental_income_log/airport_rental_income_log.js
+++ airplane_mode/shop_lease/doctype/airport_rental_income_log/airport_rental_income_log.js
@@ -2,12 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Airport Rental Income Log", {
-    refresh(frm){
-        
-    },
+	shop_rental_contract(frm) {
+		if (frm.doc.shop_rental_contract) {
+			frm.call({
+				method: "get_dates",
+				doc: frm.doc,
+				callback: function(r) {
+					if (r.message) {
+						frm.set_value("period_start", r.message.period_start);
+						frm.set_value("period_end", r.message.period_end);
+					}
+				}
+			});
+		}
+	},
 	status(frm) {
         if (frm.doc.status == "Received") {
-            frm.set_value('payment_received_date', new Date());
+            frm.set_value('payment_received_date', frappe.datetime.now_date());
         } else {
             frm.set_value('payment_received_date', null);
         }
--- airplane_mode/shop_lease/doctype/airport_rental_income_log/airport_rental_income_log.py
+++ airplane_mode/shop_lease/doctype/airport_rental_income_log/airport_rental_income_log.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils import add_days, add_months
 
 
 class AirportRentalIncomeLog(Document):
@@ -11,3 +12,40 @@ class AirportRentalIncomeLog(Document):
 		if self.status != "Received":
 			frappe.throw(_('Document can only be submitted after payment received.'))
 
+	def before_validate(self):
+		if not self.period_start or not self.period_end:
+			dates = self.get_dates()
+			self.period_start = dates.get("period_start")
+			self.period_end = dates.get("period_end")
+
+	@frappe.whitelist()
+	def get_dates(self):
+		dates = {}
+		last_period_end = frappe.db.get_value(self.doctype,
+			filters={
+				'shop_rental_contract' : self.shop_rental_contract,
+				'status' : 'Received',
+				'name': ['!=', self.name]
+			},
+			fieldname='period_end',
+			order_by='period_end DESC'
+		)
+		if last_period_end:
+			start_date = add_days(last_period_end, 1)
+		else:
+			start_date = frappe.db.get_value(
+				'Shop Rental Contract',
+				self.shop_rental_contract,
+				'effective_date'
+			)
+		
+		next_month = add_months(start_date, 1)
+		end_date = add_days(next_month, -1)
+		dates = {
+			'period_start': start_date,
+			'period_end': end_date
+		}
+		return dates
+	
+
+	
