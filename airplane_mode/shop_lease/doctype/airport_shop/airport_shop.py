fd0ff5711cf42da7421e4692df521d71820be348
william-kung <william0129@gmail.com>
2026-03-11 12:32:29 +0800

    add web view for airport shop

--- airplane_mode/shop_lease/doctype/airport_shop/airport_shop.py
+++ airplane_mode/shop_lease/doctype/airport_shop/airport_shop.py
@@ -1,13 +1,13 @@
 # Copyright (c) 2026, DDR and contributors
 # For license information, please see license.txt
 
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 from frappe.model.naming import getseries
 import frappe
 
 
 
-class AirportShop(Document):
+class AirportShop(WebsiteGenerator):
 	def autoname(self):
 		airport_code = self.airport_code
 		shop_number = self.shop_number
