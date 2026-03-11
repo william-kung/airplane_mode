470f0eb7cdc3777c546554dc2dea72d1dfbe653e
william-kung <william0129@gmail.com>
2026-03-11 12:31:12 +0800

    give up on crew flights to be displayed on single crew form

--- airplane_mode/airplane_mode/doctype/flight_crew_member/flight_crew_member.js
+++ airplane_mode/airplane_mode/doctype/flight_crew_member/flight_crew_member.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Flight Crew Member", {
-// 	refresh(frm) {
+// 	flights(frm) {
+//         frappe.call("get_flights")
+//         .then (r =>{
+//             if (r.message) {
+//                 frm.set_value('flights', r.message);
 
-// 	},
+//             }
+//         })
+// 	}
 // });
--- airplane_mode/airplane_mode/doctype/flight_crew_member/flight_crew_member.py
+++ airplane_mode/airplane_mode/doctype/flight_crew_member/flight_crew_member.py
@@ -1,9 +1,43 @@
 # Copyright (c) 2026, DDR and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cached_property
 
 
 class FlightCrewMember(Document):
-	pass
+    pass
+
+
+
+    # @property
+    # def flights(self):
+    #     flight_names = frappe.get_all(
+    #         "Flight Crew On Board",
+    #         filters={"flight_crew_member": self.name},
+    #         pluck="parent",
+    #     )
+    #     frappe.msgprint(flight_names)
+    #     return frappe.get_all(
+    #         "Airplane Flight",
+    #         filters={"name": ["in", flight_names]},
+    #         fields=["name", "source_airport_code", "destination_airport_code", "date_of_departure"],
+    #     )
+
+	# @frappe.whitelist()
+	# def get_flights(self:dict | None = None):
+	# 	frappe.msgprint(self.name)
+	# 	flight_names = frappe.get_all(
+	# 		"Flight Crew On Board",
+	# 		filters={"flight_crew_member": self.name},
+	# 		pluck="parent",
+	# 	)
+	# 	frappe.msgprint(flight_names[0])
+
+	# 	flights = frappe.get_all("Airplaine Flight",
+	# 		filters = {"name": {"in": flight_names}},
+	# 		fields = ['name', 'source_airport_code', 'destination_airport_code', 'date_of_departure']
+	# 	)
+
+	# 	return flights
