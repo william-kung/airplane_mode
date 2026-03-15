6b9fd3ab1a08631b2fdeaa0ec38320f3cbd0a340
william-kung <william0129@gmail.com>
2026-03-15 17:27:49 +0800

    add rename logic on shop rental contract

--- airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.py
+++ airplane_mode/shop_lease/doctype/shop_rental_contract/shop_rental_contract.py
@@ -4,10 +4,16 @@
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import getdate, add_days, add_months
+from frappe.model.naming import getseries
+from frappe.utils import add_days, add_months
 
 
 class ShopRentalContract(Document):
+	def autoname(self):
+		prefix = f"{self.airport_code}-S{self.shop_number}"
+		series = getseries(prefix, 3)
+		self.name = f"{prefix}-{series}"
+
 	def validate(self):
 		self.verify_rent_amount()
 		self.check_overlap_dates()
@@ -15,42 +21,47 @@ class ShopRentalContract(Document):
 	def on_submit(self):
 		self.create_income_tracking()
 
-
+	def on_update(self):
+		if self.has_value_changed("airport_shop"):
+			self.rename()
 	
 	def check_overlap_dates(self):
-		
-		effective_date = getdate(self.effective_date)
-		expiry_date = getdate(self.expiry_date)
+		base_filters = {
+			'name': ['!=', self.name],
+			'airport_shop': self.airport_shop,
+		}
+		or_filters = [
+			["effective_date", "between", [self.effective_date, self.expiry_date]],
+			["expiry_date", "between", [self.effective_date, self.expiry_date]]
+		]
 
-
-		contracts = frappe.db.get_list("Shop Rental Contract",
-			filters={
-				'airport_shop': self.airport_shop,
-			},
-			fields=['name', 'effective_date', 'expiry_date', 'docstatus']
+		overlapped = frappe.db.get_list("Shop Rental Contract",
+			filters = base_filters,
+			or_filters = or_filters,
+			fields = ['docstatus'],
+			order_by = "docstatus desc",
+			limit = 1
 		)
 
-		for c in contracts:
-			if self.name == c.name:
-				continue
-			c_start = getdate(c.effective_date)
-			c_end = getdate(c.expiry_date)
-			
-			if (effective_date >= c_start and effective_date <= c_end) or (expiry_date >= c_start and expiry_date <= c_end):
-				if c.docstatus == 1:
-					frappe.throw(
+		if overlapped:
+			if overlapped[0]["docstatus"] == 1:
+				frappe.throw(
 					title=_("Error"),
 					msg=_("Request denied.  This period is overlapping with another lease.  Please select another Airport Shop or amend Effective Date and Expiry Date")
 				)
-				elif c.docstatus == 0:
-					frappe.msgprint(
-						title=_("Warning"),
-						msg=_("⚠️There is another potential contract overlapping this period.  Please make sure you resolve conflict before submit."),
-						indicator="orange"
-					)
-					break
-				else:
-					continue
+			else:
+				frappe.msgprint(
+				title=_("Warning"),
+				msg=_("⚠️There is another potential contract overlapping this period.  Please make sure you resolve conflict before submit."),
+				indicator="orange"
+			)
+				
+	def rename(self):
+		new_prefix = f"{self.airport_code}-S{self.shop_number}"
+		new_series = getseries(new_prefix, 3)
+		new_name = f"{new_prefix}-{new_series}"
+		frappe.rename_doc(self.doctype, self.name, new_name)
+
 
 	
 	def create_income_tracking(self):
