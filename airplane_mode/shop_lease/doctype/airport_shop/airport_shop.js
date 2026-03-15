d91ae475f29b75e074d96c58bc562ca49b7b2996
william-kung <william0129@gmail.com>
2026-03-15 20:36:46 +0800

    add Leasing custom button on Airport shop form

--- airplane_mode/shop_lease/doctype/airport_shop/airport_shop.js
+++ airplane_mode/shop_lease/doctype/airport_shop/airport_shop.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2026, DDR and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airport Shop", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Airport Shop", {
+	refresh(frm) {
+        frm.add_custom_button(__('Check Availability'), function() {
+            // Define the report route and filters
+            frappe.set_route('query-report', 'Shop Lease by Date');
+        }, __("Leasing"));
+        frm.add_custom_button(__('Add New Shop Lease'), function() {
+            frappe.new_doc('Shop Rental Contract', {
+                airport_shop: frm.doc.name
+            });
+        }, __("Leasing"));
+    }
+});
