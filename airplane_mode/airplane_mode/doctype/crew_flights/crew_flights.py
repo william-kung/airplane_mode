94c382619e57e516c6b4387be41a7fb34aa357f6
william-kung <william0129@gmail.com>
2026-03-19 19:08:11 +0800

    delete doctype Crew Flights

--- airplane_mode/airplane_mode/doctype/crew_flights/crew_flights.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, DDR and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CrewFlights(Document):
-	pass
