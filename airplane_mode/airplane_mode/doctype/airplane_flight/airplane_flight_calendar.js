2845d6fc10a74168e90f6f04d53bacd5a2c02ffb
william-kung <william0129@gmail.com>
2026-03-27 19:49:20 +0800

    remove flight crew from airplane flight page for web view

---
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight_calendar.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2026, DDR and contributors
+// For license information, please see license.txt
+
+frappe.views.calendar["Airplane Flight"] = {
+    // field_map: {
+	// 	start: "start_date",
+	// 	end: "end_date",
+	// },
+    // gantt: true
+};
